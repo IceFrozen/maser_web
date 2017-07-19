4dd66580ca8df8c2f361cae4d697e882d30e780d
JasonLee <taozi031@163.com>
2017-07-20 01:13:08 +0800

    no message

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,5 +1,17 @@
 module.exports = {
-  appId:"wx805f60be1812cff6",
-  baseURL:"http://wx.mahjong.haowan98.com:3001/api",
-  redirectUri:"http://wx.mahjong.haowan98.com/"
+  baseURL:"http://lizhipeng.space/api"
 }
+
+/*	
+微信开放平台:
+	wxe475d3fe30b0ff1b
+	7ed710eea2c60ce246a0a95ea4e2d5ea
+
+公众平台
+	wxe6aca4dd8bfab4a9
+	2999ae35eee064298baa81b5f21ef748
+麻将平台
+	wx805f60be1812cff6
+	cfdbc4d1f9d1ebd6bd635e939065e3fa
+
+*/
